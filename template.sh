9fc429c46813fca541bd9e94cfd3b127b83ca934
sahil <sahilpate3345@gmail.com>
2025-10-25 10:06:45 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,19 @@
+#create directory
+mkdir -p src
+mkdir -p research
+#creating files
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch README.md
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "directory and files created successfully!."
+
+
+
+
